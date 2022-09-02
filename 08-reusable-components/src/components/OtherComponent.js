dce8d3867d351322ff192b774bc43f7797837895
Dmitry Taskaev <89737095+DmiTsa@users.noreply.github.com>
2022-09-02 09:49:34 +0300

    09-49--02-09-2022

---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,14 @@
+function MyComponent() {
+  return (
+    <div>
+      <p>Here is other component</p>
+      <ui>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ui>
+    </div>
+  );
+}
+export default MyComponent;
